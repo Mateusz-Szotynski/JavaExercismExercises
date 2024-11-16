322a6b541d734d23c8bb174aa52dc9c845e64fc9
Mateusz <mateusz.szotynski3@gmail.com>
2024-11-16 22:25:28 +0100

    completed exercise eliuds-eggs

--- eliuds-eggs/src/main/java/EliudsEggs.java
+++ eliuds-eggs/src/main/java/EliudsEggs.java
@@ -1,6 +1,6 @@
 public class EliudsEggs {
     public int eggCount(int number) {
-        String bits = String.valueOf(Integer.toBinaryString(number));
+        String bits = Integer.toBinaryString(number);
         int length = bits.length();
         int numberOfEggs = 0;
         for (int i = 0; i < length; i++) {
